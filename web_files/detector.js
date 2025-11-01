e323112860a802fe379ddaf1fe8ceade921bb9fe
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-10-31 18:11:35 -0600

    Update detector.js

--- web_files/detector.js
+++ web_files/detector.js
@@ -1,6 +1,6 @@
 let Detector = {
-  worker: null,
-  ready: false,
+  worker:null,
+  ready:false,
 
   async init(){
     if(!this.worker){
@@ -11,23 +11,18 @@ let Detector = {
 
   async processFrame(frame){
     if(!this.ready) return null;
-    return new Promise(resolve => {
-      this.worker.onmessage = e => {
-        if(e.data.type === "mask"){
-          resolve({
-            data: e.data.maskData,
-            width: e.data.width,
-            height: e.data.height
-          });
-        } else resolve(null);
+    return new Promise(resolve=>{
+      this.worker.onmessage=e=>{
+        if(e.data.type==="mask") resolve({data:e.data.maskData,width:e.data.width,height:e.data.height});
+        else resolve(null);
       };
-      this.worker.postMessage({ type: "process", frame });
+      this.worker.postMessage({type:"process", frame});
     });
   },
 
   applyMask(ctx, mask){
     if(!mask) return;
-    const imgData = new ImageData(new Uint8ClampedArray(mask.data), mask.width, mask.height);
-    ctx.putImageData(imgData, 0, 0);
+    const imgData=new ImageData(new Uint8ClampedArray(mask.data),mask.width,mask.height);
+    ctx.putImageData(imgData,0,0);
   }
 };
