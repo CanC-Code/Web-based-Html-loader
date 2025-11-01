b918c59904fdeb0abfdd01dfbae7b3032fa4081a
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 12:53:13 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -1,31 +1,72 @@
-importScripts('https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation.js');
+importScripts('https://docs.opencv.org/4.7.0/opencv.js');
 
-let segmentation = null;
-let frameWidth = 0, frameHeight = 0;
+onmessage = e => {
+  const { frame, mask, effect } = e.data;
+  const out = new ImageData(frame.width, frame.height);
 
-async function initSegmentation(width, height) {
-  frameWidth = width;
-  frameHeight = height;
-  segmentation = new SelfieSegmentation({
-    locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`
-  });
-  segmentation.setOptions({ modelSelection: 1, selfieMode: true });
-  segmentation.onResults(onResults);
-  postMessage({ type: 'ready' });
-}
+  // If OpenCV is loaded and effect is bgBlur, apply Gaussian blur
+  if (effect === 'bgBlur' && typeof cv !== 'undefined') {
+    let src = cv.matFromImageData(frame);
+    let blurred = new cv.Mat();
+    cv.GaussianBlur(src, blurred, new cv.Size(21,21), 0);
 
-function onResults(results) {
-  const maskImage = results.segmentationMask;
-  const tmpCanvas = new OffscreenCanvas(frameWidth, frameHeight);
-  const tmpCtx = tmpCanvas.getContext('2d');
-  tmpCtx.drawImage(maskImage, 0, 0, frameWidth, frameHeight);
-  const imageData = tmpCtx.getImageData(0, 0, frameWidth, frameHeight);
-  const mask = new Uint8ClampedArray(frameWidth * frameHeight);
-  for (let i = 0; i < mask.length; i++) mask[i] = imageData.data[i*4];
-  postMessage(mask, [mask.buffer]);
-}
+    for (let i = 0; i < mask.length; i++) {
+      const idx = i*4;
+      if (mask[i] < 128) {  // Background
+        out.data[idx] = blurred.data[idx];
+        out.data[idx+1] = blurred.data[idx+1];
+        out.data[idx+2] = blurred.data[idx+2];
+        out.data[idx+3] = 255;
+      } else {  // Foreground
+        out.data[idx] = frame.data[idx];
+        out.data[idx+1] = frame.data[idx+1];
+        out.data[idx+2] = frame.data[idx+2];
+        out.data[idx+3] = 255;
+      }
+    }
+    src.delete();
+    blurred.delete();
 
-onmessage = async e => {
-  if (e.data.type === 'init') await initSegmentation(e.data.width, e.data.height);
-  else createImageBitmap(e.data).then(img => segmentation.send({ image: img }));
+  } else {
+    // Loop through each pixel
+    for (let i = 0; i < mask.length; i++) {
+      const idx = i*4;
+      const alpha = mask[i];
+
+      switch(effect) {
+        case 'bgRemove':
+          // Foreground stays, background transparent
+          out.data[idx] = frame.data[idx];
+          out.data[idx+1] = frame.data[idx+1];
+          out.data[idx+2] = frame.data[idx+2];
+          out.data[idx+3] = alpha; // Use mask as alpha
+          break;
+
+        case 'objectExclude':
+          // Masked area becomes transparent (exclude object)
+          if (alpha > 128){ 
+            out.data[idx] = 0; 
+            out.data[idx+1] = 0; 
+            out.data[idx+2] = 0; 
+            out.data[idx+3] = 0;
+          } else {
+            out.data[idx] = frame.data[idx];
+            out.data[idx+1] = frame.data[idx+1];
+            out.data[idx+2] = frame.data[idx+2];
+            out.data[idx+3] = 255;
+          }
+          break;
+
+        default:
+          // No effect, copy original
+          out.data[idx] = frame.data[idx];
+          out.data[idx+1] = frame.data[idx+1];
+          out.data[idx+2] = frame.data[idx+2];
+          out.data[idx+3] = 255;
+          break;
+      }
+    }
+  }
+
+  postMessage(out, [out.data.buffer]);
 };
