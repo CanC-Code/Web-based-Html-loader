fef36c21e65174b9ba1a2f248daa41adb21f3a87
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 12:38:55 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -5,20 +5,18 @@ onmessage = e => {
   const out = new ImageData(frame.width, frame.height);
 
   if (effect === 'bgBlur' && typeof cv !== 'undefined') {
-    // OpenCV blur implementation for background
     let src = cv.matFromImageData(frame);
-    let maskMat = cv.matFromArray(frame.height, frame.width, cv.CV_8UC1, mask);
     let blurred = new cv.Mat();
     cv.GaussianBlur(src, blurred, new cv.Size(21,21), 0);
 
     for (let i = 0; i < mask.length; i++) {
       const idx = i*4;
-      if (mask[i] < 128) { // background
+      if (mask[i] < 128) {
         out.data[idx] = blurred.data[idx];
         out.data[idx+1] = blurred.data[idx+1];
         out.data[idx+2] = blurred.data[idx+2];
         out.data[idx+3] = 255;
-      } else { // foreground
+      } else {
         out.data[idx] = frame.data[idx];
         out.data[idx+1] = frame.data[idx+1];
         out.data[idx+2] = frame.data[idx+2];
@@ -26,39 +24,28 @@ onmessage = e => {
       }
     }
 
-    src.delete(); blurred.delete(); maskMat.delete();
+    src.delete(); blurred.delete();
   } else {
-    // bgRemove, objectExclude, or none
     for (let i = 0; i < mask.length; i++) {
       const idx = i*4;
       const alpha = mask[i];
 
       switch(effect) {
         case 'bgRemove':
-          // Keep foreground, background transparent
           out.data[idx] = frame.data[idx];
           out.data[idx+1] = frame.data[idx+1];
           out.data[idx+2] = frame.data[idx+2];
           out.data[idx+3] = alpha;
           break;
-
         case 'objectExclude':
-          // Remove object (alpha > 128)
           if(alpha > 128){ 
-            out.data[idx] = 0; 
-            out.data[idx+1] = 0; 
-            out.data[idx+2] = 0; 
-            out.data[idx+3] = 0; 
-          } else { 
-            out.data[idx] = frame.data[idx]; 
-            out.data[idx+1] = frame.data[idx+1]; 
-            out.data[idx+2] = frame.data[idx+2]; 
-            out.data[idx+3] = 255; 
+            out.data[idx] = 0; out.data[idx+1] = 0; out.data[idx+2] = 0; out.data[idx+3] = 0;
+          } else {
+            out.data[idx] = frame.data[idx]; out.data[idx+1] = frame.data[idx+1];
+            out.data[idx+2] = frame.data[idx+2]; out.data[idx+3] = 255;
           }
           break;
-
         default:
-          // No effect: pass frame as-is
           out.data[idx] = frame.data[idx];
           out.data[idx+1] = frame.data[idx+1];
           out.data[idx+2] = frame.data[idx+2];
@@ -68,6 +55,5 @@ onmessage = e => {
     }
   }
 
-  // Send processed frame back to main thread
   postMessage(out, [out.data.buffer]);
 };
