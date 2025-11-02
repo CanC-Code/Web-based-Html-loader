812c280ac7238f742f12f1cd259713eb84244453
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 21:41:13 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -1,48 +1,32 @@
-// processor-worker.js
-
 let history = [];
-const FRAME_HISTORY = 2; // Number of previous frames for temporal blending
+const FRAME_HISTORY = 2;
 
 self.onmessage = async (e) => {
   const msg = e.data;
-
   if (msg.type === 'frame') {
     const { bitmap, mask } = msg;
 
-    // Convert mask to ImageBitmap
     const maskBitmap = await createImageBitmap(mask);
-
-    // Offscreen canvas for processing
     const off = new OffscreenCanvas(bitmap.width, bitmap.height);
     const offCtx = off.getContext('2d');
 
-    // Draw original video frame
     offCtx.drawImage(bitmap, 0, 0);
 
-    // Apply feathered mask
     offCtx.save();
     offCtx.globalCompositeOperation = 'destination-in';
-    offCtx.filter = 'blur(2px)'; // Feathering
+    offCtx.filter = 'blur(2px)'; // automated feathering
     offCtx.drawImage(maskBitmap, 0, 0, bitmap.width, bitmap.height);
     offCtx.restore();
 
-    // Temporal blending
     const frameData = offCtx.getImageData(0, 0, bitmap.width, bitmap.height);
-    history.push(frameData.data.slice()); // copy
-
+    history.push(frameData.data.slice());
     if (history.length > FRAME_HISTORY) history.shift();
 
     if (history.length > 1) {
-      // Blend frames to reduce flicker
       const blended = new Uint8ClampedArray(frameData.data.length);
       for (let i = 0; i < blended.length; i += 4) {
         let r = 0, g = 0, b = 0, a = 0;
-        history.forEach(f => {
-          r += f[i];
-          g += f[i+1];
-          b += f[i+2];
-          a += f[i+3];
-        });
+        history.forEach(f => { r += f[i]; g += f[i+1]; b += f[i+2]; a += f[i+3]; });
         const count = history.length;
         blended[i] = r / count;
         blended[i+1] = g / count;
@@ -52,11 +36,8 @@ self.onmessage = async (e) => {
       frameData.data.set(blended);
     }
 
-    // Convert processed frame to ImageBitmap
     const processedBitmap = await createImageBitmap(frameData);
-
     self.postMessage({ type: 'frame', bitmap: processedBitmap }, [processedBitmap]);
-
   } else if (msg.type === 'finish') {
     self.postMessage({ type: 'done' });
   }
