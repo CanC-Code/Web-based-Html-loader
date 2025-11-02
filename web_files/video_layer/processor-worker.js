24ec8b76a2bd79a5aa2dc604d240185af7ab99dd
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 18:59:46 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -1,9 +1,36 @@
+// Temporal Anti-Aliasing + Multi-frame Mask Blending
+const FRAME_HISTORY = 3; // number of previous frames to blend
+let history = [];
+
 self.onmessage = e => {
   const msg = e.data;
   if(msg.type==='blend'){
-    const {width,height} = msg;
-    const frame = new ImageData(new Uint8ClampedArray(msg.data),width,height);
-    // basic temporal AA / smoothing can be applied here
-    postMessage({type:'frame',data:frame.data.buffer},[frame.data.buffer]);
+    const { width, height } = msg;
+    const frameData = new Uint8ClampedArray(msg.data);
+
+    // Store current frame in history
+    history.push(frameData);
+    if(history.length > FRAME_HISTORY) history.shift();
+
+    // Initialize blended frame
+    const blended = new Uint8ClampedArray(frameData.length);
+    
+    // Blend frames
+    for(let i=0; i<frameData.length; i+=4){
+      let r=0,g=0,b=0,a=0;
+      history.forEach(f => {
+        r += f[i];
+        g += f[i+1];
+        b += f[i+2];
+        a += f[i+3];
+      });
+      const count = history.length;
+      blended[i]   = r / count;
+      blended[i+1] = g / count;
+      blended[i+2] = b / count;
+      blended[i+3] = a / count;
+    }
+
+    postMessage({ type:'frame', data:blended.buffer }, [blended.buffer]);
   }
 };
