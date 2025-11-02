e89f0b9ba344c8fd93ddb27fdcdbfdf807b7bd29
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 20:09:01 -0600

    Update detector-worker.js

--- web_files/video_layer/detector-worker.js
+++ web_files/video_layer/detector-worker.js
@@ -3,25 +3,26 @@ let history = [];
 
 self.onmessage = e => {
   const msg = e.data;
-  if(msg.type === 'blend'){
+  if(msg.type==='blend'){
     const { width, height, data } = msg;
     const frameData = new Uint8ClampedArray(data);
 
-    history.push(frameData);
+    history.push(frameData.slice());
     if(history.length > FRAME_HISTORY) history.shift();
 
     const blended = new Uint8ClampedArray(frameData.length);
 
+    const allFrames = [...history, frameData];
+    const count = allFrames.length;
+
     for(let i=0;i<frameData.length;i+=4){
       let r=0,g=0,b=0,a=0;
-      history.forEach(f => { r+=f[i]; g+=f[i+1]; b+=f[i+2]; a+=f[i+3]; });
-      const count = history.length;
-      blended[i]=r/count;
-      blended[i+1]=g/count;
-      blended[i+2]=b/count;
-      blended[i+3]=a/count;
+      allFrames.forEach(f=>{
+        r+=f[i]; g+=f[i+1]; b+=f[i+2]; a+=f[i+3];
+      });
+      blended[i]=r/count; blended[i+1]=g/count; blended[i+2]=b/count; blended[i+3]=a/count;
     }
 
-    postMessage({ type:'frame', data:blended.buffer }, [blended.buffer]);
+    postMessage({type:'frame', data:blended.buffer}, [blended.buffer]);
   }
 };
