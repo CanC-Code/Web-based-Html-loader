52d590ade9a3a4b6d7455394f3820af53ee6c70d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 18:09:02 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -1,43 +1,9 @@
-importScripts('https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation@0.1/selfie_segmentation.js');
-
-let selfieSeg;
-self.onerror = e => postMessage({ type: 'error', message: e.message });
-
-async function initSegmentation() {
-  selfieSeg = new SelfieSegmentation({ locateFile: f => `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${f}` });
-  selfieSeg.setOptions({ modelSelection: 1 });
-  await selfieSeg.initialize();
-}
-
-initSegmentation();
-
-onmessage = async e => {
+self.onmessage = e => {
   const msg = e.data;
-  if (msg.type === 'process') {
-    if (!selfieSeg) {
-      postMessage({ type: 'error', message: 'Segmentation model not initialized.' });
-      return;
-    }
-    try {
-      const { width, height } = msg;
-      const imgData = new ImageData(new Uint8ClampedArray(msg.data), width, height);
-      const off = new OffscreenCanvas(width, height);
-      const ctx = off.getContext('2d');
-      ctx.putImageData(imgData, 0, 0);
-
-      const results = await selfieSeg.send({ image: off });
-      const mask = results.segmentationMask;
-
-      const comp = new OffscreenCanvas(width, height);
-      const cctx = comp.getContext('2d');
-      cctx.drawImage(off, 0, 0);
-      cctx.globalCompositeOperation = 'destination-in';
-      cctx.drawImage(mask, 0, 0, width, height);
-
-      const final = cctx.getImageData(0, 0, width, height);
-      postMessage({ type: 'frame', data: final.data.buffer }, [final.data.buffer]);
-    } catch (err) {
-      postMessage({ type: 'error', message: err.message });
-    }
+  if (msg.type === 'blend') {
+    const { width, height } = msg;
+    const frame = new ImageData(new Uint8ClampedArray(msg.data), width, height);
+    // basic smoothing or light enhancement can happen here if needed
+    postMessage({ type: 'frame', data: frame.data.buffer }, [frame.data.buffer]);
   }
 };
