61ca3a91d19e5a963706320f32745de6119d9b64
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 19:08:27 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -1,26 +1,37 @@
 const FRAME_HISTORY = 3;
-let history=[];
+let history = [];
 
-self.onmessage = e=>{
+self.onmessage = e => {
   const msg = e.data;
-  if(msg.type==='blend'){
-    const { width, height } = msg;
-    const frameData = new Uint8ClampedArray(msg.data);
+  if (msg.type === 'blend') {
+    const { width, height, data, feather } = msg;
+    const frameData = new Uint8ClampedArray(data);
 
+    // Store history
     history.push(frameData);
-    if(history.length>FRAME_HISTORY) history.shift();
+    if (history.length > FRAME_HISTORY) history.shift();
 
+    // Blend frames for temporal anti-aliasing
     const blended = new Uint8ClampedArray(frameData.length);
-    for(let i=0;i<frameData.length;i+=4){
-      let r=0,g=0,b=0,a=0;
-      history.forEach(f=>{
-        r+=f[i]; g+=f[i+1]; b+=f[i+2]; a+=f[i+3];
+    for (let i = 0; i < frameData.length; i += 4) {
+      let r = 0, g = 0, b = 0, a = 0;
+      history.forEach(f => {
+        r += f[i]; g += f[i + 1]; b += f[i + 2]; a += f[i + 3];
       });
       const count = history.length;
-      blended[i] = r/count; blended[i+1] = g/count;
-      blended[i+2] = b/count; blended[i+3] = a/count;
+      blended[i] = r / count;
+      blended[i + 1] = g / count;
+      blended[i + 2] = b / count;
+      blended[i + 3] = a / count;
     }
 
-    postMessage({type:'frame',data:blended.buffer},[blended.buffer]);
+    // Optional feathering
+    if (feather > 0) {
+      for (let i = 3; i < blended.length; i += 4) {
+        blended[i] = blended[i] * 0.95 + 255 * (feather / 20);
+      }
+    }
+
+    postMessage({ type: 'frame', data: blended.buffer }, [blended.buffer]);
   }
 };
