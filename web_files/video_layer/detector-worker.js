879e80c531b74d312649fbb3b2526fee3d72e44b
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 19:08:42 -0600

    Update detector-worker.js

--- web_files/video_layer/detector-worker.js
+++ web_files/video_layer/detector-worker.js
@@ -1,8 +1,14 @@
-// Can be extended to handle motion detection / multi-model fusion
+importScripts('opencv.js');
+
 self.onmessage = e => {
   const msg = e.data;
-  if(msg.type==='process'){
-    // currently placeholder, processing handled in processor-worker.js
-    postMessage({type:'done'});
+  if (msg.type === 'detect') {
+    const { width, height, data } = msg;
+    const frame = new cv.Mat(height, width, cv.CV_8UC4, new Uint8Array(data));
+    // Placeholder: apply OpenCV processing for motion/edge detection
+    // For now, just pass through
+    const output = new Uint8ClampedArray(frame.data);
+    frame.delete();
+    postMessage({ type: 'mask', data: output.buffer }, [output.buffer]);
   }
 };
