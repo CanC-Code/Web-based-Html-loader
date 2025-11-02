92887a53ce5758058c64bfe42858672b983658ec
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 18:04:30 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -1,44 +1,43 @@
-importScripts('https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation.js');
+importScripts('https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation@0.1/selfie_segmentation.js');
 
-let segmenter;
-let width = 0, height = 0;
-let offCanvas, offCtx;
+let selfieSeg;
+self.onerror = e => postMessage({ type: 'error', message: e.message });
 
-onmessage = async (e) => {
-  const { type, data } = e.data;
+async function initSegmentation() {
+  selfieSeg = new SelfieSegmentation({ locateFile: f => `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${f}` });
+  selfieSeg.setOptions({ modelSelection: 1 });
+  await selfieSeg.initialize();
+}
 
-  if (type === 'init') {
-    width = e.data.width;
-    height = e.data.height;
-    offCanvas = new OffscreenCanvas(width, height);
-    offCtx = offCanvas.getContext('2d');
+initSegmentation();
 
-    segmenter = new SelfieSegmentation({
-      locateFile: f => `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${f}`
-    });
-    segmenter.setOptions({ modelSelection: 1 });
-    segmenter.onResults(results => handleResults(results));
+onmessage = async e => {
+  const msg = e.data;
+  if (msg.type === 'process') {
+    if (!selfieSeg) {
+      postMessage({ type: 'error', message: 'Segmentation model not initialized.' });
+      return;
+    }
+    try {
+      const { width, height } = msg;
+      const imgData = new ImageData(new Uint8ClampedArray(msg.data), width, height);
+      const off = new OffscreenCanvas(width, height);
+      const ctx = off.getContext('2d');
+      ctx.putImageData(imgData, 0, 0);
 
-    postMessage({ type: 'ready' });
-    return;
-  }
+      const results = await selfieSeg.send({ image: off });
+      const mask = results.segmentationMask;
 
-  if (type === 'frame' && segmenter) {
-    const img = new ImageData(new Uint8ClampedArray(data.data), width, height);
-    offCtx.putImageData(img, 0, 0);
-    await segmenter.send({ image: offCanvas });
+      const comp = new OffscreenCanvas(width, height);
+      const cctx = comp.getContext('2d');
+      cctx.drawImage(off, 0, 0);
+      cctx.globalCompositeOperation = 'destination-in';
+      cctx.drawImage(mask, 0, 0, width, height);
+
+      const final = cctx.getImageData(0, 0, width, height);
+      postMessage({ type: 'frame', data: final.data.buffer }, [final.data.buffer]);
+    } catch (err) {
+      postMessage({ type: 'error', message: err.message });
+    }
   }
 };
-
-function handleResults(results) {
-  const mask = results.segmentationMask;
-  offCtx.save();
-  offCtx.clearRect(0, 0, width, height);
-  offCtx.drawImage(mask, 0, 0, width, height);
-  offCtx.globalCompositeOperation = 'source-in';
-  offCtx.drawImage(results.image, 0, 0, width, height);
-  offCtx.restore();
-
-  const output = offCtx.getImageData(0, 0, width, height);
-  postMessage({ type: 'render', data: output.data, width, height }, [output.data.buffer]);
-}
