9d12f82ece94cc3a709907d0b87e003819318a5f
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 19:00:39 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -1,36 +1,26 @@
-// Temporal Anti-Aliasing + Multi-frame Mask Blending
-const FRAME_HISTORY = 3; // number of previous frames to blend
-let history = [];
+const FRAME_HISTORY = 3;
+let history=[];
 
-self.onmessage = e => {
+self.onmessage = e=>{
   const msg = e.data;
   if(msg.type==='blend'){
     const { width, height } = msg;
     const frameData = new Uint8ClampedArray(msg.data);
 
-    // Store current frame in history
     history.push(frameData);
-    if(history.length > FRAME_HISTORY) history.shift();
+    if(history.length>FRAME_HISTORY) history.shift();
 
-    // Initialize blended frame
     const blended = new Uint8ClampedArray(frameData.length);
-    
-    // Blend frames
-    for(let i=0; i<frameData.length; i+=4){
+    for(let i=0;i<frameData.length;i+=4){
       let r=0,g=0,b=0,a=0;
-      history.forEach(f => {
-        r += f[i];
-        g += f[i+1];
-        b += f[i+2];
-        a += f[i+3];
+      history.forEach(f=>{
+        r+=f[i]; g+=f[i+1]; b+=f[i+2]; a+=f[i+3];
       });
       const count = history.length;
-      blended[i]   = r / count;
-      blended[i+1] = g / count;
-      blended[i+2] = b / count;
-      blended[i+3] = a / count;
+      blended[i] = r/count; blended[i+1] = g/count;
+      blended[i+2] = b/count; blended[i+3] = a/count;
     }
 
-    postMessage({ type:'frame', data:blended.buffer }, [blended.buffer]);
+    postMessage({type:'frame',data:blended.buffer},[blended.buffer]);
   }
 };
