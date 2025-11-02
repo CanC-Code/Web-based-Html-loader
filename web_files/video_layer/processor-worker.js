7368e02820f8a755b5a4eb5478c83165ad8bbf13
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 19:15:03 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -1,37 +1,39 @@
-const FRAME_HISTORY = 3;
-let history = [];
+const FRAME_HISTORY=3;
+let history=[];
 
-self.onmessage = e => {
-  const msg = e.data;
-  if (msg.type === 'blend') {
-    const { width, height, data, feather } = msg;
-    const frameData = new Uint8ClampedArray(data);
+self.onmessage=e=>{
+  const msg=e.data;
+  if(msg.type==='blend'){
+    const {width,height,feather}=msg;
+    let frame=new Uint8ClampedArray(msg.data);
+    const mask=new Uint8ClampedArray(msg.mask);
 
-    // Store history
-    history.push(frameData);
-    if (history.length > FRAME_HISTORY) history.shift();
+    // Apply mask to frame
+    for(let i=0;i<frame.length;i+=4){
+      const alpha=mask[i+3]/255;
+      frame[i+0]*=alpha;
+      frame[i+1]*=alpha;
+      frame[i+2]*=alpha;
+      frame[i+3]=255;
+    }
 
-    // Blend frames for temporal anti-aliasing
-    const blended = new Uint8ClampedArray(frameData.length);
-    for (let i = 0; i < frameData.length; i += 4) {
-      let r = 0, g = 0, b = 0, a = 0;
-      history.forEach(f => {
-        r += f[i]; g += f[i + 1]; b += f[i + 2]; a += f[i + 3];
+    // Temporal AA + multi-frame blending
+    history.push(frame);
+    if(history.length>FRAME_HISTORY) history.shift();
+
+    const blended=new Uint8ClampedArray(frame.length);
+    for(let i=0;i<frame.length;i+=4){
+      let r=0,g=0,b=0,a=0;
+      history.forEach(f=>{
+        r+=f[i]; g+=f[i+1]; b+=f[i+2]; a+=f[i+3];
       });
-      const count = history.length;
-      blended[i] = r / count;
-      blended[i + 1] = g / count;
-      blended[i + 2] = b / count;
-      blended[i + 3] = a / count;
+      const count=history.length;
+      blended[i]=r/count;
+      blended[i+1]=g/count;
+      blended[i+2]=b/count;
+      blended[i+3]=a/count;
     }
 
-    // Optional feathering
-    if (feather > 0) {
-      for (let i = 3; i < blended.length; i += 4) {
-        blended[i] = blended[i] * 0.95 + 255 * (feather / 20);
-      }
-    }
-
-    postMessage({ type: 'frame', data: blended.buffer }, [blended.buffer]);
+    self.postMessage({type:'frame',data:blended.buffer},[blended.buffer]);
   }
 };
