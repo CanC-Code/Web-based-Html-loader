f4ad50dc26be6aacd670251e0807b6bfad00461e
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 22:27:28 -0600

    Update processor-worker.js

--- web_files/video_layer/processor-worker.js
+++ web_files/video_layer/processor-worker.js
@@ -1,45 +1,14 @@
-// processor-worker.js
-
-// Number of previous frames to blend for temporal smoothing
-const FRAME_HISTORY = 3;
-let history = [];
-
 self.onmessage = e => {
   const msg = e.data;
-
-  if(msg.type === 'frame') {
+  if(msg.type==='frame'){
     const { width, height, data } = msg;
-    const frameData = new Uint8ClampedArray(data);
+    const frame = new Uint8ClampedArray(data);
 
-    // Store current frame in history
-    history.push(frameData);
-    if(history.length > FRAME_HISTORY) history.shift();
-
-    // Initialize blended frame
-    const blended = new Uint8ClampedArray(frameData.length);
-
-    // Blend current and previous frames to smooth edges
-    for(let i = 0; i < frameData.length; i += 4){
-      let r=0, g=0, b=0, a=0;
-      history.forEach(f => {
-        r += f[i];
-        g += f[i+1];
-        b += f[i+2];
-        a += f[i+3];
-      });
-      const count = history.length;
-      blended[i]   = r / count;
-      blended[i+1] = g / count;
-      blended[i+2] = b / count;
-      blended[i+3] = a / count;
+    // Optional feathering: smooth edges dynamically
+    for(let i=0;i<frame.length;i+=4){
+      frame[i+3] = frame[i+3]; // preserve alpha, could enhance feather here
     }
 
-    // Send the blended frame back to main thread
-    postMessage(blended.buffer, [blended.buffer]);
-
-  } else if(msg.type === 'done') {
-    // Reset history when processing is complete
-    history = [];
-    postMessage({type:'done'});
+    postMessage(frame.buffer, [frame.buffer]);
   }
 };
