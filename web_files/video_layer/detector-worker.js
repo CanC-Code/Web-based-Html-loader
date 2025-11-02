6af45b48b034d369e41981dc478fce0b4a344aea
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-11-01 19:50:31 -0600

    Update detector-worker.js

--- web_files/video_layer/detector-worker.js
+++ web_files/video_layer/detector-worker.js
@@ -1,11 +1,10 @@
-// Worker: applies temporal AA + feathering + mask blending
 const FRAME_HISTORY = 3;
 let history = [];
 
 self.onmessage = e => {
   const msg = e.data;
-  if(msg.type==='blend'){
-    const { width, height, data, feather } = msg;
+  if(msg.type === 'blend'){
+    const { width, height, data } = msg;
     const frameData = new Uint8ClampedArray(data);
 
     history.push(frameData);
@@ -13,24 +12,14 @@ self.onmessage = e => {
 
     const blended = new Uint8ClampedArray(frameData.length);
 
-    for(let i=0; i<frameData.length; i+=4){
+    for(let i=0;i<frameData.length;i+=4){
       let r=0,g=0,b=0,a=0;
-      history.forEach(f=>{
-        r += f[i]; g += f[i+1]; b += f[i+2]; a += f[i+3];
-      });
+      history.forEach(f => { r+=f[i]; g+=f[i+1]; b+=f[i+2]; a+=f[i+3]; });
       const count = history.length;
-      blended[i]   = r/count;
-      blended[i+1] = g/count;
-      blended[i+2] = b/count;
-      blended[i+3] = a/count;
-    }
-
-    // Feathering (simple box blur)
-    if(feather>0){
-      // very light smoothing per pixel
-      for(let i=0;i<blended.length;i+=4){
-        blended[i+3] = blended[i+3] * (1 - feather/30); // alpha softening
-      }
+      blended[i]=r/count;
+      blended[i+1]=g/count;
+      blended[i+2]=b/count;
+      blended[i+3]=a/count;
     }
 
     postMessage({ type:'frame', data:blended.buffer }, [blended.buffer]);
